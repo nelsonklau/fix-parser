a7387bf8b6e8cb5b31e7b4161079fb0926f375b9
nelsonlau <nelson.kf.lau@gmail.com>
2025-05-06 23:59:46 +0800

    #1 re-arrange utilities

--- src/main/java/com/jpmorgan/fix/Parser.java
+++ src/main/java/com/jpmorgan/fix/Parser.java
@@ -1,25 +1,14 @@
 package com.jpmorgan.fix;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 public final class Parser {
-
-    private final static byte SOH = 0x01;
-    private final static byte EQUALS = 0x3d;
 
     private final Stack<Float> currentGroup = new Stack<>();
     private final Map<Integer, Integer> groupOccurrences = new HashMap<>(60, 1);
 
     public Parser() {
-        Stream.of(
-                534,  78, 870, 420, 398, 576, 938, 936, 518, 382,
-                862,  85, 864, 124, 627, 146, 555, 428, 199, 558,
-                670, 604, 683,  33,  73, 268, 267, 816, 136, 384,
-                539, 756, 948, 806, 952, 804, 453, 211, 753, 702,
-                711, 802, 295, 735, 296, 510, 473, 215, 454, 778,
-                781, 801, 552, 232, 580, 897, 386, 768, 887, 457
-        ).forEach(group -> groupOccurrences.put(group, 0));
+        GROUP_TAGS.forEach(group -> groupOccurrences.put(group, 0));
     }
 
     /**
@@ -97,10 +86,54 @@ public final class Parser {
         return result;
     }
 
+    // Delimiters to split FIX message into tag-value fields
+
+    private final static byte SOH = 0x01;
+    private final static byte EQUALS = 0x3d;
+
+    // Utilities to convert byte array to numeric types
+
+    private final static Map<Byte, Float> DIGITS = Map.of(
+            (byte) 0x30, 0.0f,
+            (byte) 0x31, 1.0f,
+            (byte) 0x32, 2.0f,
+            (byte) 0x33, 3.0f,
+            (byte) 0x34, 4.0f,
+            (byte) 0x35, 5.0f,
+            (byte) 0x36, 6.0f,
+            (byte) 0x37, 7.0f,
+            (byte) 0x38, 8.0f,
+            (byte) 0x39, 9.0f
+    );
+
+    private int parseToInt(List<Byte> value) {
+        int result = 0;
+        for (int i = value.size() - 1, exponent = 0; i >= 0; i--)
+            result += DIGITS.get(value.get(i)).intValue() * (int) Math.pow(10, exponent++);
+        return result;
+    }
+
+    private float parseTag(Deque<Byte> buf, float memberIndex) {
+        Byte b;
+        int exponent = 0;
+        float tag = 0;
+        while ((b = buf.pollLast()) != null)
+            tag += DIGITS.get(b) * (float) Math.pow(10, exponent++);
+        return tag + memberIndex;
+    }
+
     // Utilities to manage groups in metaspace
 
     private final static Map<Integer, Set<Integer>> GROUP_MEMBER_DEFINITIONS = new HashMap<>(57, 1);
     private final static Map<Integer, Set<Integer>> OPTIONAL_GROUP_MEMBER_DEFINITIONS = new HashMap<>(57, 1);
+    private final static Set<Integer> GROUP_TAGS = Set.of(
+                534,  78, 870, 420, 398, 576, 938, 936, 518, 382,
+                862,  85, 864, 124, 627, 146, 555, 428, 199, 558,
+                670, 604, 683,  33,  73, 268, 267, 816, 136, 384,
+                539, 756, 948, 806, 952, 804, 453, 211, 753, 702,
+                711, 802, 295, 735, 296, 510, 473, 215, 454, 778,
+                781, 801, 552, 232, 580, 897, 386, 768, 887, 457
+    );
 
     static {
         Set.of(
@@ -257,36 +290,5 @@ public final class Parser {
         OPTIONAL_GROUP_MEMBER_DEFINITIONS.put(938, Set.of(896));
         OPTIONAL_GROUP_MEMBER_DEFINITIONS.put(948, Set.of(949, 950, 951));
         OPTIONAL_GROUP_MEMBER_DEFINITIONS.put(952, Set.of(953, 954));
-    }
-
-    // Utilities to convert byte array to numeric types
-
-    private final static Map<Byte, Float> DIGITS = Map.of(
-            (byte) 0x30, 0.0f,
-            (byte) 0x31, 1.0f,
-            (byte) 0x32, 2.0f,
-            (byte) 0x33, 3.0f,
-            (byte) 0x34, 4.0f,
-            (byte) 0x35, 5.0f,
-            (byte) 0x36, 6.0f,
-            (byte) 0x37, 7.0f,
-            (byte) 0x38, 8.0f,
-            (byte) 0x39, 9.0f
-    );
-
-    private int parseToInt(List<Byte> value) {
-        int result = 0;
-        for (int i = value.size() - 1, exponent = 0; i >= 0; i--)
-            result += DIGITS.get(value.get(i)).intValue() * (int) Math.pow(10, exponent++);
-        return result;
-    }
-
-    private float parseTag(Deque<Byte> buf, float memberIndex) {
-        Byte b;
-        int exponent = 0;
-        float tag = 0;
-        while ((b = buf.pollLast()) != null)
-            tag += DIGITS.get(b) * (float) Math.pow(10, exponent++);
-        return tag + memberIndex;
     }
 }
